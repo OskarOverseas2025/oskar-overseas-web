fc73a791fd913578af3e6650d7d90aa3d1c2b6b9
Shred Zeppelin <gogurung.bardan@gmail.com>
2026-06-01 12:45:18 +0545

    removed whatsapp number

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -56,19 +56,17 @@ export default function ContactPage() {
                       </div>
                     </div>
 
-                    <div className="flex items-start space-x-3">
+                    {/* <div className="flex items-start space-x-3">
                       <Phone className="h-5 w-5 text-green-600 mt-1" />
                       <div>
                         <h3 className="font-semibold text-gray-900">
                           Phone Numbers
                         </h3>
                         <p className="text-gray-600">
-                          {/* Mobile: +977 9863801109 */}
-                          {/* <br /> */}
                           WhatsApp: +44 7472 498254
                         </p>
                       </div>
-                    </div>
+                    </div> */}
 
                     <div className="flex items-start space-x-3">
                       <Mail className="h-5 w-5 text-orange-600 mt-1" />
